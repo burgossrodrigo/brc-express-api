77f1b13d9a8ed8eb1de09060237c5f6909651e45
Rodrigo Burgos <drigo@tangleswap.exchange>
2023-05-11 13:58:47 -0300

    mongodb

--- methods/CRUD.js
+++ methods/CRUD.js
@@ -1,3 +1,9 @@
+const { MongoClient, ServerApiVersion } = require('mongodb')
+
+const uri = process.env.MONGODB_URI
+
+const mongoClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 const queryTokens = async () => {
     try {
         const collection = await mongoClient.db("BRC").collection("tokens");
