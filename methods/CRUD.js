78cd682d2403761871cfdce121e77434fae591fc
Rodrigo Burgos <76675806+burgossrodrigo@users.noreply.github.com>
2023-05-12 17:01:41 -0300

    working

--- methods/CRUD.js
+++ methods/CRUD.js
@@ -1,4 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb')
+const { get24hChange, getLastPrice, get24hVolumeChange, getLastVolume, get24hMarketCapChange, getLastMarketCap } = require('./')
 
 const uri = 'mongodb+srv://burgossrodrigo:BeREmhPli0p3qFTq@tangle.hkje2xt.mongodb.net/?retryWrites=true&w=majority'
 
@@ -6,13 +7,25 @@ const mongoClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopo
 
 const queryTokens = async () => {
     try {
-      const collection = await mongoClient.db("BRC").collection("tokens");
-      const documents = await collection.find({}).limit(50).toArray();
-      return documents;
+        let tokens = []
+        const collection = await mongoClient.db("BRC").collection("tokens")
+        const documents = await collection.find({}).toArray()
+        for(const token of documents){
+            tokens.push({
+                name: token.token,
+                priceChange24h: get24hChange(token.price),
+                lastPrice: getLastPrice(token.price),
+                volumeChange24h: get24hVolumeChange(token.volume),
+                lastVolume: getLastVolume(token.volume),
+                marketCapChange24h: get24hMarketCapChange(token.marketCap),
+                lastMarketCap: getLastMarketCap(token.marketCap),
+                marketCapArr: token.marketCap
+            })
+        }
+        return tokens
     } catch (error) {
-      console.log(error.message, 'for queryTokens');
-      return [];
+        console.log(error.message, 'for queryTokens')
     }
-  };
+}
 
 module.exports = { queryTokens }
--- methods/index.js
+++ methods/index.js
@@ -1,49 +1,87 @@
 const getCandlestickData = (data, resolution) => {
-    const ohlcData = []
-    let currentOHLC = null
-    // Sort the data by timestamp
-    data.sort((a, b) => new Date(a.time).getTime() - new Date(b.time).getTime())
-  
-    // Loop through each data point and calculate the OHLC values for the time interval
-    for (let i = 0; i < data.length; i++) {
-      const currentData = data[i]
-      const timestamp = new Date(currentData.time).getTime()
-  
-      // If the current data point has a price value, update the OHLC values
-      if (currentData.price) {
-        if (!currentOHLC) {
-          // If there is no current OHLC data, start a new time interval
-          currentOHLC = {
-            open: currentData.price,
-            high: currentData.price,
-            low: currentData.price,
-            close: currentData.price,
-            timestamp
-          }
-        } else if (timestamp - currentOHLC.timestamp >= 3600000 * resolution) {
-          // If the current data point is outside the current time interval, push the current OHLC data and start a new time interval
-          ohlcData.push(currentOHLC)
-          currentOHLC = {
-            open: currentData.price,
-            high: currentData.price,
-            low: currentData.price,
-            close: currentData.price,
-            timestamp
-          }
-        } else {
-          // Update the current OHLC data for the time interval
-          currentOHLC.high = Math.max(currentOHLC.high, currentData.price)
-          currentOHLC.low = Math.min(currentOHLC.low, currentData.price)
-          currentOHLC.close = currentData.price
+  const ohlcData = []
+  let currentOHLC = null
+  // Sort the data by timestamp
+  data.sort((a, b) => new Date(a.time).getTime() - new Date(b.time).getTime())
+
+  // Loop through each data point and calculate the OHLC values for the time interval
+  for (let i = 0; i < data.length; i++) {
+    const currentData = data[i]
+    const timestamp = new Date(currentData.time).getTime()
+
+    // If the current data point has a price value, update the OHLC values
+    if (currentData.price) {
+      if (!currentOHLC) {
+        // If there is no current OHLC data, start a new time interval
+        currentOHLC = {
+          open: currentData.price,
+          high: currentData.price,
+          low: currentData.price,
+          close: currentData.price,
+          timestamp
         }
+      } else if (timestamp - currentOHLC.timestamp >= 3600000 * resolution) {
+        // If the current data point is outside the current time interval, push the current OHLC data and start a new time interval
+        ohlcData.push(currentOHLC)
+        currentOHLC = {
+          open: currentData.price,
+          high: currentData.price,
+          low: currentData.price,
+          close: currentData.price,
+          timestamp
+        }
+      } else {
+        // Update the current OHLC data for the time interval
+        currentOHLC.high = Math.max(currentOHLC.high, currentData.price)
+        currentOHLC.low = Math.min(currentOHLC.low, currentData.price)
+        currentOHLC.close = currentData.price
       }
     }
-  
-    // Push the final OHLC data point
-    if (currentOHLC) {
-      ohlcData.push(currentOHLC)
-    }
-    return ohlcData
   }
 
-  module.exports = { getCandlestickData }
+  // Push the final OHLC data point
+  if (currentOHLC) {
+    ohlcData.push(currentOHLC)
+  }
+  return ohlcData
+}
+
+const calculateChange = (oldValue, newValue) => ((newValue - oldValue) / oldValue) * 100;
+
+const get24hChange = (data) => {
+  const now = new Date();
+  const oneDayAgo = new Date(now.getTime() - (24 * 60 * 60 * 1000));
+
+  const data24hAgo = data.find((entry) => new Date(entry.time) >= oneDayAgo);
+
+  if (!data24hAgo) {
+    return null;
+  }
+
+  const latestData = data[data.length - 1];
+
+  return calculateChange(parseFloat(data24hAgo.price), parseFloat(latestData.price));
+};
+
+const getLastPrice = (priceData) => {
+  return parseFloat(priceData[priceData.length - 1].price);
+};
+
+const get24hVolumeChange = (volumeData) => {
+  return get24hChange(volumeData);
+};
+
+const getLastVolume = (volumeData) => {
+  return parseFloat(volumeData[volumeData.length - 1].volume);
+};
+
+const get24hMarketCapChange = (marketCapData) => {
+  return get24hChange(marketCapData);
+};
+
+const getLastMarketCap = (marketCapData) => {
+  return parseFloat(marketCapData[marketCapData.length - 1].marketCap);
+};
+
+
+module.exports = { getCandlestickData, getLastPrice, get24hVolumeChange, getLastVolume, get24hMarketCapChange, getLastMarketCap, get24hChange }
